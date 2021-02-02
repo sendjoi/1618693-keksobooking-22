3a6f4bff1493d80cd7b3fe3face63a1844ff0a0d
Sergey <fox.rsp@gmail.com>
2021-02-03 01:00:18 +0300

    Начал делать генерацию данных

--- js/main.js
+++ js/main.js
@@ -1,4 +1,46 @@
+const = typeInput ['palace', 'flat',  'house', 'bungalow'];
+const = checkinInpit ['12:00', '13:00', '14:00'];
+const = checkoutInpit ['12:00', '13:00', '14:00'];
+const featuresInput = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photosInput = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
 const  getRandomNumber =  function (min, max, numberLength) {
-    return +(Math.random() * (max - min) + min).toFixed(numberLength); 
+  return Number((Math.random() * (max - min) + min).toFixed(numberLength));
+}
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomNumber(0, elements.length -1, 0)];
+};
+
+const offerSamples = {
+    author: {
+      avatar: img/avatars/user{{xx}}.png
+    },
+    offer: {
+      title: "Best housing",
+      address: [location.x, location.y],
+      price: getRandomNumber(0,100,0),
+      type: getRandomArrayElement(typeInput),
+      rooms: getRandomNumber(0,100,0),
+      guests: getRandomNumber(0,100,0),
+      checkin: getRandomArrayElement(checkinInpit),
+      checkout: getRandomArrayElement(checkoutInpit),
+      features: getRandomArrayElement(featuresInput),
+      description: "The best as you can find",
+      photos: getRandomArrayElement(photosInput),
+    },
+    location: {
+      x: getRandomNumber(35.65000, 35.70000, 5),
+      y: getRandomNumber(139.70000, 139.80000, 5)
+    }
+}
+
+const OFFERS_QUANTITY = 10;
+
+  const createOffer = () => {
+    return  {
+
+    }
   }
-  
+
+  const allOffers = new Array(OFFERS_QUANTITY).fill(null).map(() => createOffer());

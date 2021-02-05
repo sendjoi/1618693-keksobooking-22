7a2ac7af199bae1eae6194eb0674de82f83800a6
Sergey <fox.rsp@gmail.com>
2021-02-05 19:52:05 +0300

    Правки строчки и переменных

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 const TITLES = ['Hotel Ukraine', 'Hostel Friens', 'House Island', 'Apartament Castle'];
 const ADDRESS = ['Baker street 5c8', 'Red Square 2c3', 'Main Avenue 4c1', 'Green boulevard 5c9'];
 const TYPE = ['palace', 'flat',  'house', 'bungalow'];
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
+const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const DESCRIPTIONS = ['Best comfort in center', 'Twice cleaning', 'Low noize, delicious breakfast ', 'Of couse you can with animals'];
 const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const OFFERS_QUANTITY = 10;
-const XMINLIMIT = 35.60000;
-const XMAXLIMIT = 35.70000;
-const YMINLIMIT = 139.70000;
-const YMAXLIMIT = 139.80000;
-const XYFLOAT = 5;
+const X_MIN_LIMIT = 35.60000;
+const X_MAX_LIMIT = 35.70000;
+const Y_MIN_LIMIT = 139.70000;
+const Y_MAX_LIMIT = 139.80000;
+const X_Y_FLOAT = 5;
 
 // Функция рандомного числа
 const  getRandomNumber =  function (min, max, numberLength) {
@@ -33,7 +33,7 @@ const getRandomArray = (elements) => {
 const createOffer = () => {
   return  {
     author: {
-      avatar: 'img/avatars/`user0${getRandomNumber(1, 8, 0)}`.png'
+      avatar: `img/avatars/user0${getRandomNumber(1, 8, 0)}.png`
       },
     offer: {
       title: getRandomArrayElement(TITLES),
@@ -42,15 +42,15 @@ const createOffer = () => {
       type: getRandomArrayElement(TYPE),
       rooms: getRandomNumber(1,10,0),
       guests: getRandomNumber(1,20,0),
-      checkin: getRandomArrayElement(CHECKIN),
-      checkout: getRandomArrayElement(CHECKOUT),
+      checkin: getRandomArrayElement(CHECKIN_TIMES),
+      checkout: getRandomArrayElement(CHECKOUT_TIMES),
       features: getRandomArray(FEATURES),
       description: getRandomArrayElement(DESCRIPTIONS),
       photos: getRandomArray(PHOTOS),
       },
     location: {
-      x: getRandomNumber(XMINLIMIT, XMAXLIMIT, XYFLOAT),
-      y: getRandomNumber(YMINLIMIT, YMAXLIMIT, XYFLOAT)
+      x: getRandomNumber(X_MIN_LIMIT, X_MAX_LIMIT, X_Y_FLOAT),
+      y: getRandomNumber(Y_MIN_LIMIT, Y_MAX_LIMIT, X_Y_FLOAT)
       }
   }
 }

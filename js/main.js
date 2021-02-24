16f4285655be95ed7126f37e75d2817e9d375000
Sergey <fox.rsp@gmail.com>
2021-02-24 18:51:03 +0300

    Добавил карту

--- js/main.js
+++ js/main.js
@@ -1,15 +1,43 @@
 import  {addingOffers} from './create-offers.js';
 import './form.js'
+import { fillPhotos } from './util.js';
 
-/*
+
 const form = document.querySelector('.ad-form');
-const filters = document.querySelector('.map__filters');
-
-const housingType = document.querySelector('.housing-type');
-
-housingType.disabaled = true;
-
 form.classList.add('ad-form--disabled');
-filters.classList.add('.map__filters--disabled');
 
-*/
+const filters = document.querySelector('.map__filters');
+filters.classList.add('map__filters--disabled')
+
+
+const filter = document.querySelectorAll('select');
+const inputs = document.querySelectorAll('input');
+const textArea = document.querySelectorAll('textarea');
+
+[...filter].forEach((element) => {
+  element.disabled = true;
+});
+
+[...inputs].forEach((element) => {
+  element.disabled = true;
+});
+
+[...textArea].forEach((element) => {
+  element.disabled = true;
+});
+
+
+
+/* global L:readonly */
+const map = L.map('map-canvas')
+  .setView({
+    lat: 59.92749,
+    lng: 30.31127,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);

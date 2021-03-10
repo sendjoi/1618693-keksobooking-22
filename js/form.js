a969c75cd4d7039d6ef849634d2cdaea8a5987d5
Sergey <fox.rsp@gmail.com>
2021-03-10 21:45:08 +0300

    Правки валидации

--- js/form.js
+++ js/form.js
@@ -15,6 +15,13 @@ const priceMap = {
   bungalow: '6000',
 }
 
+const minPriceMap = {
+  palace: 10000,
+  flat: 1000,
+  house: 5000,
+  bungalow: 0,
+}
+
 housingType.addEventListener('change', (event) => {
   price.value = priceMap[event.target.value]
 })
@@ -35,14 +42,55 @@ offerPriceInput.addEventListener('input', () => {
   const valueLength = offerPriceInput.value;
 
   if (valueLength > MAX_PRICE) {
-    offerPriceInput.setCustomValidity('Максимальная цена ' + MAX_PRICE).reportValidity();
+    offerPriceInput.setCustomValidity('Максимальная цена ' + MAX_PRICE);
+  } else if (valueLength > minPriceMap[housingType.value]) {
+    offerPriceInput.setCustomValidity('Максимальная цена данного типа жилья' + minPriceMap[housingType.value]);
   } else {
     offerPriceInput.setCustomValidity('');
   }
 });
 
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const offerTitleInput = document.querySelector('#title');
 
 
+offerTitleInput.addEventListener('invalid', () => {
+
+  if (offerTitleInput.validity.valueMissing) {
+    offerTitleInput.setCustomValidity('Обязательное поле');
+  } else {
+    offerTitleInput.setCustomValidity('');
+  }
+});
+
+offerTitleInput.addEventListener('input', () => {
+  const valueLength = offerTitleInput.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    offerTitleInput.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) +' симв.');
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    offerTitleInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) +' симв.');
+  } else {
+    offerTitleInput.setCustomValidity('');
+  }
+});
+
+const offerRoomsInput = document.querySelector('#room_number');
+
+
+
+offerRoomsInput.addEventListener('change', (evt) => {
+  if (evt.target.value === '1') {
+    document.querySelector('#capacity').setCustomValidity('Только для 1 гостя');
+  } else if (evt.target.value === '2') {
+    document.querySelector('#capacity').setCustomValidity('Для 2 гостей или для 1 гостя');
+  } else if (evt.target.value === '3') {
+    document.querySelector('#capacity').setCustomValidity('Для 3 гостей, для 2 гостей или для 1 гостя');
+  } else if (evt.target.value === '100') {
+    document.querySelector('#capacity').setCustomValidity('Не для гостей');
+  }
+})
 
 
 timeIn.addEventListener('change', () => {
@@ -73,30 +121,6 @@ offerForm.addEventListener('submit', (evt) => {
     })
 })
 
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-const offerTitleInput = document.querySelector('#title');
 
-
-offerTitleInput.addEventListener('invalid', () => {
-
-  if (offerTitleInput.validity.valueMissing) {
-    offerTitleInput.setCustomValidity('Обязательное поле');
-  } else {
-    offerTitleInput.setCustomValidity('');
-  }
-});
-
-offerTitleInput.addEventListener('input', () => {
-  const valueLength = offerTitleInput.value.length;
-
-  if (valueLength < MIN_TITLE_LENGTH) {
-    offerTitleInput.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) +' симв.');
-  } else if (valueLength > MAX_TITLE_LENGTH) {
-    offerTitleInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) +' симв.');
-  } else {
-    offerTitleInput.setCustomValidity('');
-  }
-});
 
 export {putAddressinInput};
